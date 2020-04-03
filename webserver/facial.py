ea41e309abef88c4b27f06388ce013a379f94a85
James <jamholliday@live.co.uk>
2020-04-03 13:24:57 +0100

    added deletion of images automatically - Only facial recognition data is stored for ethics

--- webserver/facial.py
+++ webserver/facial.py
@@ -5,20 +5,32 @@ import numpy as np
 from numpy import savetxt
 import pickle
 import json
+import shutil
+
+all_face_encodings = {}
+current_face_encodings = {}
+
+if os.path.exists('dataset_faces.dat'):
+    with open('dataset_faces.dat', 'rb') as f:
+        current_face_encodings = pickle.load(f)
 
 # Load the jpg file into a numpy array
 i = 1
 directory = './public/images/'
 known_face_names = []
-all_face_encodings = {}
+
 for subdir, dirs, files in os.walk(directory):
     for file in files:
         print((os.path.join(subdir, file)))
         image = face_recognition.load_image_file(os.path.join(subdir, file))
-        all_face_encodings[os.path.basename(subdir)] = face_recognition.face_encodings(image)[0]
+        current_face_encodings[os.path.basename(subdir)] = face_recognition.face_encodings(image)[0]
+
 
 with open('dataset_faces.dat', 'wb') as f:
-    pickle.dump(all_face_encodings, f)
+    pickle.dump(current_face_encodings, f)
+
+if os.path.basename(subdir) != "images"
+    shutil.rmtree(subdir)
 
 # Find all the faces in the image using the default HOG-based model.
 # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
